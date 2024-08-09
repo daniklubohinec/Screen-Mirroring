4f2e0d9c09ed75af9a7ad578f1e779a90f9f53a9
daniklubohinec <danik.lubohinec@gmail.com>
2024-08-09 17:44:50 +0300

    Initial commit

---
+++ Screen-MirroringUITests/Screen_MirroringUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Screen_MirroringUITestsLaunchTests.swift
+//  Screen-MirroringUITests
+//
+//  Created by Danik Lubohinec on 9.08.24.
+//
+
+import XCTest
+
+final class Screen_MirroringUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
